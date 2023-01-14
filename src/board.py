da8332c482cc156ebe94c39cc7e96e3f156272ee
Nathan Relyea <nathan.r.relyea@gmail.com>
2023-01-13 23:07:43 -0800

    pre-movement
    base setup, pre-movement and pre-refactor integrating chess package

---
+++ src/board.py
@@ -0,0 +1,24 @@
+from const import *
+from square import Square
+from piece import *
+
+class Board:
+
+    def __init__(self):
+        self.squares = [[0,0,0,0,0,0,0,0] for col in range(COLS)]
+
+        self._create()
+        self._add_pieces('white')
+        self._add_pieces('black')
+
+    def _create(self):
+        for row in range(ROWS):
+            for col in range(COLS):
+                self.squares[row][col] = Square(row, col)
+
+    def _add_pieces(self, color):
+
+        row_pawn, row_other = (6,7) if color == 'white' else (1,0)
+
+        for col in range(COLS):
+            self.squares[row_pawn][col] = Square(row_pawn, col, Pawn(color))
---
+++ src/const.py
@@ -0,0 +1,13 @@
+# Screen dimensions
+WIDTH = 800
+HEIGHT = 800
+
+# Board dimensions
+ROWS = 8
+COLS = 8
+
+SQSIZE = WIDTH // COLS
+
+# Board colors
+LIGHTCOLOR = (234,235,200)
+DARKCOLOR = (119,154,88)

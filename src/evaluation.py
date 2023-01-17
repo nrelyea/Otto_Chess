0e499f7af04582e6a567f771eef9a9c0a4b766aa
Nathan Relyea <nathan.r.relyea@gmail.com>
2023-01-16 20:54:04 -0800

    Basic MinMax alg working!

--- src/evaluation.py
+++ src/evaluation.py
@@ -11,7 +11,72 @@ class Evaluation:
 
     def suggest_move(self, board):
         #return self.RANDOM_MOVE(board)
-        return self.CAPTURE_PIECES(board)
+        #return self.CAPTURE_PIECES(board)
+        return self.MIN_MAX(board)
+
+
+
+    def MIN_MAX(self, board):
+
+        depth = 4       # depth = how many moves ahead to look (must be greated than 1)
+
+        legalMovesList = list(board.legal_moves)  
+
+        moveValueList = []
+
+        for move in legalMovesList:
+            board.push(move)
+            moveValueList.append(self._position_value(board, depth - 1))
+            board.pop()
+
+        #just for debug / looking
+        #for i in range(0,len(legalMovesList)):
+            #print('move ' + str(legalMovesList[i]) + ' value: ' + str(moveValueList[i]))
+        
+
+        if board.turn == chess.WHITE:
+            index_max = max(range(len(moveValueList)), key=moveValueList.__getitem__)
+            return str(legalMovesList[index_max])            
+        else:
+            index_min = min(range(len(moveValueList)), key=moveValueList.__getitem__)
+            return str(legalMovesList[index_min])
+
+
+
+
+        #legalMovesList = list(board.legal_moves)        
+        #return str(legalMovesList[0])
+    
+
+    def _position_value(self, board, depth):
+
+        if board.is_checkmate():
+            if board.turn:  # if white has been mated
+                return -99
+            else:           # if black has been mated
+                return 99
+        
+        if depth < 1:
+            return self.material_balance(board)
+
+        legalMovesList = list(board.legal_moves)
+        
+        boardValueList = []
+
+        for move in legalMovesList:
+            board.push(move)
+            boardValueList.append(self._position_value(board, depth - 1))         
+            board.pop()
+        
+        
+        if board.turn == chess.WHITE:
+            return max(boardValueList)         
+        else:
+            return min(boardValueList)    
+        
+
+        
+                
 
 
 
@@ -26,6 +91,12 @@ class Evaluation:
         
         for move in legalMovesList:
             board.push(move)
+
+            # return move if immediately determined it is mate in 1
+            if board.is_checkmate(): 
+                board.pop()
+                return str(move)
+
             moveMatBalanceList.append(self.material_balance(board))
             board.pop()
         
@@ -43,6 +114,8 @@ class Evaluation:
 
 
 
+
+
     def RANDOM_MOVE(self, board):
         legalMovesList = list(board.legal_moves)
 
@@ -51,6 +124,10 @@ class Evaluation:
         #print('current mat balance: ' + str(self.material_balance(board)))
 
         return str(legalMovesList[r])
+
+
+
+
 
 
     def material_balance(self, board):
